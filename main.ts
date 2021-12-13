ca4e2d8ef569dede541ebc6bb8fb1ad98add30ec
mitchelbunk <71022184+mitchelbunk@users.noreply.github.com>
2021-12-13 14:28:33 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,42 @@
 namespace SpriteKind {
     export const map = SpriteKind.create()
 }
-let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    assets.animation`BoatAnimationL`,
+    500,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    assets.animation`BoatAnimationR`,
+    500,
+    true
+    )
+})
+let mySprite2: Sprite = null
+let mySprite = sprites.create(assets.image`myImage`, SpriteKind.map)
+mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    . . . . . f 1 f 1 f . . . . . . 
+    . . . . . f 1 f 1 f f . . . . . 
+    . . . . f 1 8 f 8 1 f . . . . . 
+    . . . . f 8 1 f 1 8 1 f . . . . 
+    . . . f 1 8 1 f 1 8 1 f f . . . 
+    . . . f 1 8 1 f 1 8 8 1 f . . . 
+    . . f 1 8 1 1 f 1 1 1 8 1 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . f . . . . . . . . 
+    . f f f f f f f f f f f f f f . 
+    . . f 8 1 8 1 8 1 8 1 8 1 f . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite2)
+controller.moveSprite(mySprite2, 20, 20)
+scene.setBackgroundColor(6)
